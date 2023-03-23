f59894a668145ac0f82eecd20b262751918c3712
dangdinhvan <untilyou1235@gmail.com>
2023-03-23 10:17:41 +0700

    lesson-5

--- src/lesson-5/server.js
+++ src/lesson-5/server.js
@@ -1,17 +1,37 @@
-const express = require('express');
-const Controller1 = require('./features/feature1/controllers');
-const app = express();
-const port = 8080;
+// const express = require('express');
+// const app = express();
+// const port = 8080;
+//
+// app.get('/', (req, res) => {
+// 	res.send('This is home!');
+// });
+//
+// app.get('/about', (req, res) => {
+// 	res.send('This is about!');
+// });
+//
+// app.listen(port, () => {
+// 	console.log(`Example app listening on port ${port}`);
+// });
 
-app.get('/', (req, res) => {
-	const controller1 = new Controller1();
-	controller1.create();
+const fs = require('fs');
+const key = fs.readFileSync('cert/CA/localhost/localhost.decrypted.key');
+const cert = fs.readFileSync('cert/CA/localhost/localhost.crt');
+
+const express = require('express');
+const app = express();
+const https = require('https');
+const server = https.createServer({ key, cert }, app);
+
+const port = 8080;
+server.listen(port, () => {
+	console.log(`Server is listening on https://localhost:${port}`);
+});
+
+app.get('/', (req, res, next) => {
+	res.status(200).send('This is Home');
 });
 
 app.get('/about', (req, res) => {
 	res.send('This is about!');
-});
-
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
 });
